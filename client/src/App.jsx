f7d7840030326a512cf076acbc69f5680e2f9282
Sandeep Thadiparthi <102729565+SandeepThadiparthi@users.noreply.github.com>
2023-08-26 18:41:24 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+import React,{useState,useEffect} from "react";
+import {Container,AppBar,Typography,Grow,Grid} from "@material-ui/core";
+import memories from "./images/memories.png";
+import Form from "./Components/Form/form";
+import Posts from "./Components/Posts/posts";
+import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+import {getPosts} from "./actions/posts";
+
+function App(){
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+useEffect(() =>{dispatch(getPosts());} 
+  , [currentId,dispatch]);
+    return (
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">
+                Memories
+                </Typography>
+                <img src={memories} alt="memories.png" height="60"/>
+            </AppBar>
+            <Grow in>
+                <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts  setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                    </Grid>
+                </Grid>
+            </Grow>
+        </Container>
+    )
+}
+
+export default App;
+
---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const UPDATE = 'UPDATE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const LIKE = 'LIKE';
+export const DELETE = 'DELETE';
+export const CREATE = 'CREATE';
